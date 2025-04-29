5efa70c1fbdda747b1476b951afcdee7abce5568
dennislduffy <dennislduffy@gmail.com>
2025-04-29 15:04:16 -0500

    created monthly_coal to pull monthly coal generation

--- main.py
+++ main.py
@@ -6,11 +6,7 @@
 import os
 from dotenv import load_dotenv
 from scripts.electricity_generation import fetch_generation
-
-
-
-
-# Run electricity_generation.py
+from scripts.monthly_coal import fetch_monthly_coal
 
 # Define main
 
@@ -24,6 +20,9 @@ def main():
 
     # fetch generation data
     fetch_generation(eia_key, destdir)
+
+    # fetch monthly coal generation data
+    fetch_monthly_coal(eia_key, destdir)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
---
+++ scripts/monthly_coal.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from requests import get
+
+def fetch_monthly_coal(eia_key: str, destdir: str):
+
+    # query EIA for monthly coal generation data in Minnesota
+    response = get(
+        "https://api.eia.gov/v2/electricity/electric-power-operational-data/data/?api_key=" + eia_key + "&frequency=monthly&data[0]=generation&facets[fueltypeid][]=COW&facets[location][]=MN&facets[sectorid][]=99&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000").json()
+
+    # normalize response from EIA
+    coal_data = pd.json_normalize(response['response']['data'])
+
+    # grab columns that we need and convert data types
+
+    coal_data = coal_data[['period', 'generation']]
+    coal_data['period'] = pd.to_datetime(coal_data['period'], format='%Y-%m')
+    coal_data = coal_data.astype({'generation': float})
+
+    # write file
+    coal_data.to_csv(destdir + '/monthly_coal_generation.csv', index=False)
