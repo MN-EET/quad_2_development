fbbca9acab20d5f0a5db3b66f020424924644c2a
dennislduffy <dennislduffy@gmail.com>
2025-06-03 10:39:43 -0500

    updated generator build programs

--- duckdb_storage/create_generator_db.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-con = duckdb.connect("generator_database.duckdb")
-
-con.execute("""
-    CREATE TABLE solar_archive AS
-    SELECT * FROM read_csv_auto('C:/Users/dduffy/OneDrive - State of Minnesota - MN365/EIA Data/solar_generators_source.csv')
-""")
