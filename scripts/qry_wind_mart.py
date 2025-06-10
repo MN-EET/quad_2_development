261d7efdabec0e6723190c5acb407e61e4279a2d
dennislduffy <dennislduffy@gmail.com>
2025-06-10 13:34:08 -0500

    created script to query wind capacity and export

---
+++ scripts/qry_wind_mart.py
@@ -0,0 +1,11 @@
+import duckdb
+import pandas
+
+def fetch_wind_capacity(db: str, destdir: str):
+    db_con = duckdb.connect(db) #filepath to production duckdb database in duckdb_storage
+
+    # query mart_combined__wind_capacity
+    wind_capacity = db_con.execute(f'SELECT * FROM main.mart_total__wind_capacity').fetchdf()
+    db_con.close()
+    # Write dataframe to destdir
+    wind_capacity.to_csv(destdir + "/wind_capacity.csv", index = False)
